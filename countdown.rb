2cb9733d33631635d805e0949b81493a725aace7
Stephen McBride <stevemcbride3@gmail.com>
2020-04-14 17:08:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,10 +3,10 @@
 def countdown(seconds)
   i = seconds
   while i > 0
-    puts "#{i} SECOND(S)!"
+    "#{i} SECOND(S)!"
     i -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 countdown(10)
